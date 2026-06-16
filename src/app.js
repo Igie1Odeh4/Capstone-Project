c885171ee91ef35d3d4b2f431b33ef7e03138608
Omokpaogbee Patrick <patruyi@gmail.com>
2026-06-16 08:19:56 +0100

    added home path

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import authRoutes from "./routes/auth.Routes.js";
 import courseRoutes from "./routes/course.routes.js";
@@ -21,6 +26,10 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home.html"));
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/courses", courseRoutes);
